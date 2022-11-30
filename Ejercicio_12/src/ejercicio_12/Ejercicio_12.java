fca694c801a3b1f8e2191fef2d795ff0816711cf
Daniel Roa <droa@apgslt.com>
2022-11-29 23:14:06 -0500

    Cambios de Formato Ejercicio 14

--- Ejercicio_12/src/ejercicio_12/Ejercicio_12.java
+++ Ejercicio_12/src/ejercicio_12/Ejercicio_12.java
@@ -18,10 +18,48 @@ public class Ejercicio_12 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner leer = new Scanner(System.in);
+        String dato="";
+        String cadena="";
+        String mensaje="";
+        int cadenas_correctas=0;
+        int cadenas_incorrectas=0;
+        
+        
+        // Valida la extensión de la Cadena
         do{
-            System.out.println("Ingrese una cadena (5 Caracteres):");
-            String cadena = leer.nextLine();
-        }while(cadena.  );
+            do{
+                System.out.println("Ingrese una cadena (5 Caracteres):");
+                cadena = leer.nextLine();
+
+            }while(cadena.length() > 5 || cadena.length() < 0 );
+            
+            //dsempaqueta la cadea RS_232
+            
+            if(cadena.substring(0,1).equals("X") && cadena.substring(4,5).equals("O")){
+                dato = cadena.substring(1,4);
+                mensaje=mensaje+dato;
+                cadenas_correctas++;
+            }else{
+                cadenas_incorrectas++;
+            }
+            
+            
+            
+        }while(!cadena.equals("&&&&&")); // Valida el FDE
+        
+        // Validación antinegativos
+        if(cadenas_incorrectas!=0){
+            cadenas_incorrectas--;
+        }
+      
+        
+        System.out.println("Recepción Finalizada con Exito!!");
+        System.out.println(" ");
+        System.out.println("Cadenas recibidas correctamente: "+cadenas_correctas);
+        System.out.println("Cadenas recibidas incorrectamente: "+cadenas_incorrectas--);
+        System.out.println(" ");
+        System.out.println("Mensaje Recibido: "+mensaje);
+        
      }
     
 }
